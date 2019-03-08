4a25746fd8251f35e96a5dc10d55a50d60542340
pointbazaar <pointbazaar@protonmail.com>
2019-03-08 17:34:52 +0100

    make sidebar with php

--- fragments/sidebar_fragment.php
+++ fragments/sidebar_fragment.php
@@ -1,16 +1,45 @@
 <?php
     echo "sidebar";
+
+    include("../base.php");
+
+    $links = array(
+            new MenuItem("Dashboard",$baseurl . "/index.php"),
+        new MenuItem("Reports(TODO)","."),
+        new MenuItem("Sales(TODO)","."),
+        new MenuItem("Employees(TODO)","."),
+        new MenuItem("Inventory(TODO)",".")
+    );
+
+    class MenuItem{
+
+        public $name;
+        public $link;
+
+        function __construct($name,$link)
+        {
+            $this->name=$name;
+            $this->link=$link;
+        }
+    }
 ?>
 
 <ul class="list-group">
+    <?php
+        for($i=0;$i<sizeof($links);$i++){
+            echo "<a href='" . $links[$i]->link . "'>";
+                echo "<li class='list-group-item'>";
+                    echo $links[$i]->name;
+                echo "</li>";
+            echo "</a>";
+        }
+    ?>
+
+    <!--
     <a href=".">
         <li class="list-group-item">
-            Reports(TODO)
+            Reports
         </li>
     </a>
-    <a href=".">
-        <li class="list-group-item">
-            Sales(TODO)
-        </li>
-    </a>
+    -->
 </ul>
