23a8e4d46c56b7cefefb8624d5bcbdfc69fdf54c
pointbaaar <alex23667@gmail.com>
2019-03-19 13:02:36 +0100

    change to table instead of list on employee page

--- employees.php
+++ employees.php
@@ -39,26 +39,75 @@ include("authentication/is_authenticated_otherwise_redirect.php");
                 );
             ?>
 
-            <ul class="list-group">
-                <?php
-                    for($i=0;$i<sizeof($employees);$i++){
-                        echo "<li class='list-group-item'>";
-                            echo "<span>";
-                                echo($employees[$i]->toString());
-                            echo "</span>";
-                            echo "<button class='btn btn-outline-danger float-right'>";
-                                echo "Fire";
-                            echo "</button>";
-                        echo "</li>";
-                    }
-                ?>
-                <li class="list-group-item">
-                    <span>peter birne [Role: CEO]</span>
-                    <button class="btn btn-outline-danger float-right">
-                        Fire
-                    </button>
-                </li>
-            </ul>
+            <table class="">
+                <tr>
+                    <td>
+                        <p>
+                            TODO: have a form here to insert a employee
+                        </p>
+                        <form class="col">
+                            <div class="form-group">
+                                <input type="text" name="employee-name" placeholder="employee-name">
+                            </div>
+                            <div class="form-group">
+                                <input type="date" name="date-of-entry" placeholder="date-of-entry">
+                            </div>
+                            <button type="submit" value="insert employee" class="btn btn-primary">insert Employee</button>
+                        </form>
+
+                    </td>
+                </tr>
+
+            </table>
+
+            <div class="m-3 p-3"></div>
+            <div id="salestable">
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Employee Name</th>
+                        <th scope="col">Role</th>
+                        <th scope="col">Date of Entry</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        for($i=0;$i<sizeof($employees);$i++){
+                            echo "<tr>";
+                                echo("<td>");
+                                    echo($i);
+                                echo("</td>");
+
+                                echo("<td>");
+                                    echo($employees[$i]->name);
+                                echo("</td>");
+
+                                echo("<td>");
+                                    echo($employees[$i]->role);
+                                echo("</td>");
+
+                                echo("<td>");
+                                    echo("1.2.2018");
+                                echo("</td>");
+
+                                echo("<td>");
+                                    echo($employees[$i]->name  . "@gmail.com");
+                                echo("</td>");
+
+                                echo "<td>";
+                                    echo "<button class='btn btn-outline-danger'>";
+                                        echo "Delete";
+                                    echo "</button>";
+                                echo("</td>");
+                            echo "</tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
