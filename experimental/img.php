1bbd13124e686a0331d2daa5fe5e9b010da9cf85
pointbaaar <alex23667@gmail.com>
2019-03-21 15:54:19 +0100

    adjust diagram

--- experimental/img.php
+++ experimental/img.php
@@ -20,6 +20,8 @@ imagefill($im,0,0,$white);
 
 //make the x axis
 imageline($im,10,$height/2,$width,$height/2,$black);
+imagestring($im,5,0,$height/2,"0",$black);
+
 //make the y axis
 //imageline($im,10,10,10,$height,$black);
 
@@ -54,8 +56,8 @@ foreach ($my_sales as $sale){
 
 
 //top value the business ever was a
-imageline($im,50,1,$width,0,$black);
-imagestring($im,10,4,0,get_total_sales_price(),$black);
+//imageline($im,50,1,$width,0,$black);
+imagestring($im,10,$pos_x-$step_size_x,0,get_total_sales_price(),$black);
 
 function as_fraction($value,$max){
     return $value/$max;
