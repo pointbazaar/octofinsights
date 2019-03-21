61fa182f1bd5ec96856958fabd56fb6b64ec88f8
pointbaaar <alex23667@gmail.com>
2019-03-21 17:22:13 +0100

    expand inventoryservice

--- controllers/inventory/post_inventory.php
+++ controllers/inventory/post_inventory.php
@@ -2,6 +2,7 @@
 
 include_once("../../base.php");
 include_once($absolute_file_url . "/authentication/is_authenticated_otherwise_redirect.php");
+include_once($absolute_file_url . "/services/inventory_service.php");
 
 if(!$_SERVER["REQUEST_METHOD"] === "POST"){
     http_response_code(405);
@@ -13,31 +14,7 @@ session_start();
 //check for post parameters
 if(isset($_POST["item_name"]) && isset($_POST["item_price"])  && isset($_POST["amount"])  ){
 
-    echo("<p>post parameters correct</p>");
-
-    $item_name= $_POST["item_name"];
-    $item_price = $_POST["item_price"];
-    $amount = $_POST["amount"];
-
-    //insert item into inventory
-
-    include($absolute_file_url . "/database/make_database_connection.php");
-
-    try {
-
-        $conn = getConnection();
-
-        $statement = $conn->prepare("INSERT INTO inventory (item_name,item_price,amount) VALUES (:item_name,:item_price,:amount);");
-        $statement->bindParam(":item_name", $item_name);
-        $statement->bindParam(":item_price", $item_price);
-        $statement->bindParam(":amount", $amount);
-        $statement->execute();
-
-        echo("inserted item");
-
-    }catch (Exception $exception){
-        echo($exception->getMessage());
-    }
+    insert_inventory(new InventoryItem($_POST["item_name"],$_POST["item_price"],$_POST["amount"]));
 }
 
 header("Location: " . $baseurl . "/controllers/inventory/get_inventory.php");
---
+++ services/inventory_service.php
@@ -0,0 +1,58 @@
+<?php
+
+include_once($_SERVER["DOCUMENT_ROOT"] . "/octofinsights" . "/base.php");
+include_once($absolute_file_url . "/database/make_database_connection.php");
+include_once($absolute_file_url . "/model/inventory_item_entity.php");
+
+function get_all_inventory()
+{
+
+    $conn = getConnection();
+
+    $statement = $conn->prepare("SELECT * FROM inventory;");
+    $statement->execute();
+
+    $results = $statement->fetchAll();
+
+    $typed_results = array();
+
+    for($i=0;$i<sizeof($results);$i++) {
+        $item = $results[$i];
+        array_push($typed_results,new InventoryItem($item[0],$item[1],$item[2]) );
+    }
+
+    return $typed_results;
+}
+
+function insert_inventory($item){
+
+    try {
+
+        $conn = getConnection();
+
+        $statement = $conn->prepare("INSERT INTO inventory (item_name,item_price,amount) VALUES (:item_name,:item_price,:amount);");
+        $statement->bindParam(":item_name", $item->name);
+        $statement->bindParam(":item_price", $item->price);
+        $statement->bindParam(":amount", $item->amount);
+        $statement->execute();
+
+        echo("inserted item");
+
+    }catch (Exception $exception){
+        echo($exception->getMessage());
+    }
+}
+
+function delete_by_id($id){
+    try {
+
+        $conn = getConnection();
+
+        $statement = $conn->prepare("DELETE FROM inventory WHERE id=:id;");
+        $statement->bindParam(":id", $id);
+        $statement->execute();
+
+    }catch (Exception $exception){
+        echo($exception->getMessage());
+    }
+}
