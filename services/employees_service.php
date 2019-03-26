5f2066c6123d19efa85899a19dc7e56803a86130
pointbaaar <alex23667@gmail.com>
2019-03-26 13:15:33 +0100

    fix include bug

--- services/employees_service.php
+++ services/employees_service.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once($_SERVER["DOCUMENT_ROOT"] . "/octofinsights" . "/base.php");
+include_once($_SERVER["DOCUMENT_ROOT"] . "/base.php");
 include_once($_SERVER["DOCUMENT_ROOT"] . "/include_many.php");
 
 function get_all_employees()
