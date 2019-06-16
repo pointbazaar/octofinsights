13cf4d86e657b06ef46fcb46714581e5479118b5
pointbazaar <alex23667@gmail.com>
2019-06-16 18:31:42 +0200

    finally add all the jars to the classpath and begin to use JOOQ to generate classes. maybe i will finish it.

--- convert_to_java_project/src/main/java/org/vanautrui/controllers/IndexController.java
+++ convert_to_java_project/src/main/java/org/vanautrui/controllers/IndexController.java
@@ -1,5 +1,7 @@
 package org.vanautrui.controllers;
 
+import j2html.tags.ContainerTag;
+import j2html.tags.EmptyTag;
 import org.vanautrui.vaquitamvc.controller.VaquitaController;
 import org.vanautrui.vaquitamvc.controller.VaquitaTextController;
 import org.vanautrui.vaquitamvc.requests.VaquitaHTTPEntityEnclosingRequest;
@@ -9,6 +11,12 @@ import org.vanautrui.vaquitamvc.responses.VaquitaHTTPResponse;
 import org.vanautrui.vaquitamvc.responses.VaquitaRedirectResponse;
 import org.vanautrui.vaquitamvc.responses.VaquitaTextResponse;
 
+import javax.swing.text.Document;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static j2html.TagCreator.*;
 
 
 public class IndexController extends VaquitaController {
@@ -16,13 +24,58 @@ public class IndexController extends VaquitaController {
     @Override
     public VaquitaHTTPResponse handleGET(VaquitaHTTPRequest request) throws Exception {
 
+        String[] sidebar_links = new String[]{"/sales","/leads","/expenses","/employees","/inventory"};
+
         if( request.session().isPresent() && request.session().get().containsKey("authenticated") && request.session().get().get("authenticated").equals("true") ){
 
-            return new VaquitaHTMLResponse(200,"hi, "+request.session().get().get("username")+", you have logged in correctly");
+            ContainerTag sidebar = div(
+                    attrs("#sidebar .col-md-2 .sidebar .p-3 .bg-light"),
+                    a(h2("Octofinsights")).withHref("/"),
+                    div(
+                        each(
+                                Arrays.stream(sidebar_links).collect(Collectors.toList()),
+                                link->div(
+                                        attrs(".m-3"),
+                                        hr(),
+                                        a(strong(link.substring(1).toUpperCase()))
+                                                .withHref(link)
+                                )
+                        )
+                    )
+            );
+
+            String page=
+            html(
+                head(
+                    title("Octofinsights"),
+                    link().withRel("stylesheet").withHref("https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css")
+                ),
+                body(
+                    div(
+                        attrs(".row"),
+                        sidebar,
+                        div(attrs("#main-content"),
+                            h1("Hello"),
+                            p(request.session().get().get("username")),
+                            p("you have logged in correctly"),
+                            img().withSrc("/img/hello.png"),
+                            canvas(attrs("#myChart"))
+                        )
+                    ),
+                    script().withSrc("https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.js"),
+                    script().withSrc("/cashflow_chart.js")
+
+                )
+            ).render();
+
+            return new VaquitaHTMLResponse(200,page);
+
         }else {
             return new VaquitaRedirectResponse("/login.html", request);
         }
     }
+
+
 
     @Override
     public VaquitaHTTPResponse handlePOST(VaquitaHTTPEntityEnclosingRequest vaquitaHTTPEntityEnclosingRequest) throws Exception {
