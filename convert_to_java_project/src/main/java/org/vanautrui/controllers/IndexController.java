a0eced2329d74ce23bdde89c2b344a8c6a8f656b
pointbazaar <alex23667@gmail.com>
2019-06-16 17:12:30 +0200

    learn something new about redirects, that they were not supposed to change the method in the spec, but it is common practise to do so. however i want to stay away from changin the method, and want to stick to the spec.

--- convert_to_java_project/src/main/java/org/vanautrui/controllers/IndexController.java
+++ convert_to_java_project/src/main/java/org/vanautrui/controllers/IndexController.java
@@ -4,6 +4,7 @@ import org.vanautrui.vaquitamvc.controller.VaquitaController;
 import org.vanautrui.vaquitamvc.controller.VaquitaTextController;
 import org.vanautrui.vaquitamvc.requests.VaquitaHTTPEntityEnclosingRequest;
 import org.vanautrui.vaquitamvc.requests.VaquitaHTTPRequest;
+import org.vanautrui.vaquitamvc.responses.VaquitaHTMLResponse;
 import org.vanautrui.vaquitamvc.responses.VaquitaHTTPResponse;
 import org.vanautrui.vaquitamvc.responses.VaquitaRedirectResponse;
 import org.vanautrui.vaquitamvc.responses.VaquitaTextResponse;
@@ -15,8 +16,12 @@ public class IndexController extends VaquitaController {
     @Override
     public VaquitaHTTPResponse handleGET(VaquitaHTTPRequest request) throws Exception {
 
-        //return new VaquitaTextResponse(200,"hello");
-        return new VaquitaRedirectResponse("/login.html",request);
+        if( request.session().isPresent() && request.session().get().containsKey("authenticated") && request.session().get().get("authenticated").equals("true") ){
+
+            return new VaquitaHTMLResponse(200,"hi, "+request.session().get().get("username")+", you have logged in correctly");
+        }else {
+            return new VaquitaRedirectResponse("/login.html", request);
+        }
     }
 
     @Override
--- convert_to_java_project/src/main/java/org/vanautrui/controllers/LoginController.java
+++ convert_to_java_project/src/main/java/org/vanautrui/controllers/LoginController.java
@@ -3,11 +3,14 @@ package org.vanautrui.controllers;
 import org.vanautrui.vaquitamvc.controller.VaquitaController;
 import org.vanautrui.vaquitamvc.requests.VaquitaHTTPEntityEnclosingRequest;
 import org.vanautrui.vaquitamvc.requests.VaquitaHTTPRequest;
+import org.vanautrui.vaquitamvc.responses.VaquitaHTMLResponse;
 import org.vanautrui.vaquitamvc.responses.VaquitaHTTPResponse;
 import org.vanautrui.vaquitamvc.responses.VaquitaRedirectResponse;
 import org.vanautrui.vaquitamvc.responses.VaquitaTextResponse;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class LoginController extends VaquitaController {
 
@@ -16,7 +19,7 @@ public class LoginController extends VaquitaController {
     @Override
     public VaquitaHTTPResponse handleGET(VaquitaHTTPRequest vaquitaHTTPRequest) throws Exception {
 
-        return new VaquitaTextResponse(404,"not implemented");
+        return new VaquitaTextResponse(404,"not implemented GET for logincontroller");
     }
 
     @Override
@@ -27,10 +30,17 @@ public class LoginController extends VaquitaController {
 
         if(parameters.get("username").equals("test")){
             if (parameters.get("password").equals("test")){
-                return new VaquitaTextResponse(200,"logged in correctly");
+                if(vaquitaHTTPEntityEnclosingRequest.session().isPresent()){
+                    vaquitaHTTPEntityEnclosingRequest.session().get().put("authenticated","true");
+                    vaquitaHTTPEntityEnclosingRequest.session().get().put("username",parameters.get("username"));
+
+                    return new VaquitaHTMLResponse(200,"<html><a href='/'>successfully logged in. click here.</a></html>");
+                }else{
+                    return new VaquitaTextResponse(500,"something went wrong with the sessions");
+                }
             }
         }
 
-        return new VaquitaRedirectResponse("/",vaquitaHTTPEntityEnclosingRequest);
+        return new VaquitaRedirectResponse("/login.html?message=wrong_password",vaquitaHTTPEntityEnclosingRequest);
     }
 }
