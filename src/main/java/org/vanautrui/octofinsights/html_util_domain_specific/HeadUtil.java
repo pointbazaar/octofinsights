1dbfd6208d839a907e487a507cd5a42b935f2381
pointbazaar <alex23667@gmail.com>
2019-07-12 22:34:25 +0200

    change my head util to include google analytics

--- src/main/java/org/vanautrui/octofinsights/html_util_domain_specific/HeadUtil.java
+++ src/main/java/org/vanautrui/octofinsights/html_util_domain_specific/HeadUtil.java
@@ -10,11 +10,16 @@ public class HeadUtil {
 
         ContainerTag head = head(
                 head(
+                        script().withSrc("https://www.googletagmanager.com/gtag/js?id=UA-38415314-2").attr("async"),
+                        script().withSrc("mygoogleanalyticsscript.js"),
+
+                        
                         title("Octofinsights"),
                         link().withRel("stylesheet").withHref("https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"),
                         script().withSrc("https://code.jquery.com/jquery-3.3.1.slim.min.js"),
                         script().withSrc("https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"),
                         script().withSrc("https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js")
+
                 )
         );
 
