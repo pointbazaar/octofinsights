5b1d3eada412aa657e97845b883ff53eacba1d73
pointbazaar <alex23667@gmail.com>
2019-07-12 23:04:26 +0200

    change a bit about google analytics

--- src/main/java/org/vanautrui/octofinsights/html_util_domain_specific/HeadUtil.java
+++ src/main/java/org/vanautrui/octofinsights/html_util_domain_specific/HeadUtil.java
@@ -1,5 +1,6 @@
 package org.vanautrui.octofinsights.html_util_domain_specific;
 
+import j2html.attributes.Attr;
 import j2html.tags.ContainerTag;
 
 import static j2html.TagCreator.*;
@@ -10,16 +11,17 @@ public class HeadUtil {
 
         ContainerTag head = head(
                 head(
-                        script().withSrc("https://www.googletagmanager.com/gtag/js?id=UA-38415314-2").attr("async"),
-                        script().withSrc("mygoogleanalyticsscript.js"),
+                        script().withSrc("https://www.googletagmanager.com/gtag/js?id=UA-38415314-2").attr(Attr.ASYNC,"true"),
 
-                        
+
+
                         title("Octofinsights"),
                         link().withRel("stylesheet").withHref("https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"),
                         script().withSrc("https://code.jquery.com/jquery-3.3.1.slim.min.js"),
                         script().withSrc("https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"),
-                        script().withSrc("https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js")
+                        script().withSrc("https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"),
 
+                        script().withSrc("mygoogleanalyticsscript.js")
                 )
         );
 
