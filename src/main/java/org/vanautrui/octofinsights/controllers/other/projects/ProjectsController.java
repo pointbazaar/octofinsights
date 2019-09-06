4fc1ac48a8c886090feec7a6b8c449884924f5fa
pointbazaar <alex23667@gmail.com>
2019-09-07 00:48:40 +0200

    ability to archive,unarchive,delete projects.

--- src/main/java/org/vanautrui/octofinsights/controllers/other/projects/ProjectsController.java
+++ src/main/java/org/vanautrui/octofinsights/controllers/other/projects/ProjectsController.java
@@ -1,8 +1,12 @@
 package org.vanautrui.octofinsights.controllers.other.projects;
 
 import j2html.tags.ContainerTag;
+import org.jooq.DSLContext;
 import org.jooq.Record;
 import org.jooq.Result;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+import org.vanautrui.octofinsights.db_utils.DBUtils;
 import org.vanautrui.octofinsights.html_util_domain_specific.HeadUtil;
 import org.vanautrui.octofinsights.html_util_domain_specific.NavigationUtil;
 import org.vanautrui.octofinsights.services.ProjectsService;
@@ -10,15 +14,14 @@ import org.vanautrui.vaquitamvc.VaquitaApp;
 import org.vanautrui.vaquitamvc.controller.VaquitaController;
 import org.vanautrui.vaquitamvc.requests.VaquitaHTTPEntityEnclosingRequest;
 import org.vanautrui.vaquitamvc.requests.VaquitaHTTPJustRequest;
-import org.vanautrui.vaquitamvc.responses.VaquitaHTMLResponse;
-import org.vanautrui.vaquitamvc.responses.VaquitaHTTPResponse;
-import org.vanautrui.vaquitamvc.responses.VaquitaRedirectResponse;
-import org.vanautrui.vaquitamvc.responses.VaquitaTextResponse;
+import org.vanautrui.vaquitamvc.responses.*;
 
+import java.sql.Connection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static j2html.TagCreator.*;
+import static java.lang.Integer.parseInt;
 import static org.vanautrui.octofinsights.generated.tables.Projects.PROJECTS;
 
 public class ProjectsController extends VaquitaController {
@@ -31,7 +34,7 @@ public class ProjectsController extends VaquitaController {
           return new VaquitaRedirectResponse("/login",request,app);
       }
 
-      int user_id = Integer.parseInt(request.session().get().get("user_id"));
+      int user_id = parseInt(request.session().get().get("user_id"));
 
       Result<Record> myprojects = ProjectsService.getProjectsByUserId(user_id);
       List<Record> active_projects = myprojects.stream().filter(proj -> proj.get(PROJECTS.ISACTIVE).intValue() == 1).collect(Collectors.toList());
@@ -52,13 +55,13 @@ public class ProjectsController extends VaquitaController {
                                             h5("ACTIVE PROJECTS").withClasses("m-2"),
                                             ul(
 
-                                                each(active_projects,proj->makeProjectDiv(proj.get(PROJECTS.PROJECT_NAME))),
-                                                makeProjectDiv("Project 1")
+                                                each(active_projects,proj->makeProjectDiv(proj.get(PROJECTS.PROJECT_NAME),proj.get(PROJECTS.ID).intValue())),
+                                                makeProjectDiv("Project 1",-234)
                                             ).withClasses("list-group"),
                                             h5("INACTIVE PROJECTS").withClasses("m-2"),
                                             ul(
-                                                each(inactive_projects,proj->makeProjectDiv(proj.get(PROJECTS.PROJECT_NAME))),
-                                                makeInactiveProjectDiv("Project 3")
+                                                each(inactive_projects,proj->makeInactiveProjectDiv(proj.get(PROJECTS.PROJECT_NAME),proj.get(PROJECTS.ID).intValue())),
+                                                makeInactiveProjectDiv("Project 3",-234)
                                             ).withClasses("list-group")
                                         ).withClasses("col-md-12")
                                 ).withClasses("container")
@@ -67,40 +70,57 @@ public class ProjectsController extends VaquitaController {
         return new VaquitaHTMLResponse(200,page);
     }
 
-    private ContainerTag makeInactiveProjectDiv(String projectName){
+    private ContainerTag makeInactiveProjectDiv(String projectName,int project_id){
         ContainerTag res=
                 li(
-                        div(
-                                div(
-                                        s(projectName)
-                                ).withClasses("col-md-6"),
-                                div(
-                                        div(
-                                                "UNARCHIVE"
-                                        ).withClasses("btn","btn-outline-secondary","m-2"),
-                                        div(
-                                                "DELETE"
-                                        ).withClasses("btn","btn-outline-danger","m-2")
-                                ).withClasses("col-md-6","row","justify-content-end")
-                        ).withClasses("row")
+                  div(
+                    div(
+                            s(projectName)
+                    ).withClasses("col-md-6"),
+                    div(
+
+                      form(
+                        button(
+                                "UNARCHIVE"
+                        ).withClasses("btn","btn-outline-secondary","m-2")
+                      ).withAction("/projects?action=unarchive&id="+project_id).withMethod("POST"),
+
+                      form(
+                        button(
+                                "DELETE"
+                        ).withClasses("btn","btn-outline-danger","m-2")
+                      ).withAction("/projects?action=delete&id="+project_id).withMethod("POST")
+
+                    ).withClasses("col-md-6","row","justify-content-end")
+                  ).withClasses("row")
                 ).withClasses("list-group-item");
         return res;
     }
 
-    private ContainerTag makeProjectDiv(String projectName){
+    private ContainerTag makeProjectDiv(String projectName,int project_id){
         ContainerTag res=
                 li(
                     div(
                         div(
-                            projectName
+                            a(
+                              projectName
+                              //TODO: create the controller to view the project
+                            ).withHref("/projects/view?id="+project_id)
                         ).withClasses("col-md-6"),
                         div(
-                            div(
-                                "EDIT"
-                            ).withClasses("btn","btn-outline-primary","m-2"),
-                            div(
-                                "ARCHIVE"
-                            ).withClasses("btn","btn-outline-secondary","m-2")
+
+                            a(
+                              button(
+                                  "EDIT"
+                              ).withClasses("btn","btn-outline-primary","m-2")
+                              //TODO: create the edit controller (must be different than add controller, because you cannot change initial estimates, title, or start date)
+                            ).withHref("/projects/edit?id="+project_id),
+
+                            form(
+                              button(
+                                  "ARCHIVE"
+                              ).withClasses("btn","btn-outline-secondary","m-2").withType("submit")
+                            ).withAction("/projects?action=archive&id="+project_id).withMethod("POST")
                         ).withClasses("col-md-6","row","justify-content-end")
                     ).withClasses("row")
                 ).withClasses("list-group-item");
@@ -108,7 +128,50 @@ public class ProjectsController extends VaquitaController {
     }
 
     @Override
-    public VaquitaHTTPResponse handlePOST(VaquitaHTTPEntityEnclosingRequest vaquitaHTTPEntityEnclosingRequest,VaquitaApp app) throws Exception {
-        return new VaquitaTextResponse(200,"not yet implemnted");
+    public VaquitaHTTPResponse handlePOST(VaquitaHTTPEntityEnclosingRequest request,VaquitaApp app) throws Exception {
+      // this method should handle
+      // archive, unarchive, delete
+      // of projects
+
+      if( request.session().isPresent()
+              && request.session().get().containsKey("authenticated")
+              && request.session().get().get("authenticated").equals("true")
+              && request.session().get().containsKey("user_id")
+      ) {
+        int user_id = parseInt(request.session().get().get("user_id"));
+
+        int project_id = parseInt(request.getQueryParameter("id"));
+
+        String action = request.getQueryParameter("action");
+
+        Connection conn= DBUtils.makeDBConnection();
+
+        DSLContext ctx = DSL.using(conn, SQLDialect.MYSQL);
+
+        byte inactive =0;
+        byte active=1;
+
+        switch (action){
+          case "archive":
+            System.out.println("try to archive project with id "+project_id);
+            ctx.update(PROJECTS).set(PROJECTS.ISACTIVE,inactive).where(PROJECTS.ID.eq(project_id).and(PROJECTS.USER_ID.eq(user_id))).execute();
+            break;
+          case "unarchive":
+            System.out.println("try to unarchive project with id "+project_id);
+            ctx.update(PROJECTS).set(PROJECTS.ISACTIVE,active).where(PROJECTS.ID.eq(project_id).and(PROJECTS.USER_ID.eq(user_id))).execute();
+            break;
+          case "delete":
+            System.out.println("try to delete project with id "+project_id);
+            ctx.delete(PROJECTS).where(PROJECTS.ID.eq(project_id).and(PROJECTS.USER_ID.eq(user_id))).execute();
+            break;
+          default:
+            conn.close();
+            return new VaquitaTextResponse(400,"BAD REQUEST. this action is not available. ");
+        }
+        conn.close();
+        return new VaquitaRedirectToGETResponse("/projects",request);
+      }else {
+        return new VaquitaRedirectToGETResponse("/login", request);
+      }
     }
 }
