2248e8df80662950f9a19e09394c48d7914ba5c8
Marco <mastremolten2@gmail.com>
2019-10-16 12:20:42 +0200

    added tests

--- src/test/java/org/vanautrui/AppTest.java
+++ src/test/java/org/vanautrui/AppTest.java
@@ -13,4 +13,25 @@ public class AppTest
     public void test1(){
         Assert.assertTrue(true);
     }
+
+    // those tests were added with the goal to calculate a relationship between n, the amount of sessions and x, the usage of the server's RAM.
+    @Test
+    public void testOneSession(){
+        // to be added
+    }
+
+    @Test
+    public void testTenSessions(){
+        // to be added
+    }
+
+    @Test
+    public void testHundredSessions(){
+        // to be added
+    }
+
+    @Test
+    public void testThousandSessions(){
+        // to be added
+    }
 }
