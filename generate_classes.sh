85242b60acde4e276e13eb582ce6e674f9baf462
pointbazaar <alex23667@gmail.com>
2020-01-24 20:14:53 +0100

    try to fix the jooq with java 13. the java EE jars need to now be included in the classpath as they are no longer contained in the jre?

--- generate_classes.sh
+++ generate_classes.sh
@@ -1,1 +1,12 @@
-java -classpath jooq-3.11.11.jar:jooq-meta-3.11.11.jar:jooq-codegen-3.11.11.jar:mysql-connector-java-8.0.16.jar:jaxb-api-2.2.12.jar:jaxb-impl-2.3.0.jar:activation-1.1.1.jar:jaxb-core-2.2.11.jar:.  org.jooq.codegen.GenerationTool library.xml
+#https://stackoverflow.com/questions/43574426/how-to-resolve-java-lang-noclassdeffounderror-javax-xml-bind-jaxbexception-in-j
+
+java  -classpath jooq-3.11.11.jar:\
+jooq-meta-3.11.11.jar:\
+jooq-codegen-3.11.11.jar:\
+mysql-connector-java-8.0.16.jar:\
+jakarta.xml.bind-api-2.3.2:\
+jaxb-runtime-2.3.2.jar:\
+jaxb-api-2.2.12.jar:\
+jaxb-impl-2.3.0.jar:\
+activation-1.1.1.jar:\
+jaxb-core-2.2.11.jar:.  org.jooq.codegen.GenerationTool library.xml
